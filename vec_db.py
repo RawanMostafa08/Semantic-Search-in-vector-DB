78baf254ae99e83fb4d11633f3b715407a2c5d81
RawanMostafa08 <rawanmostafa401@gmail.com>
2024-12-12 16:54:09 +0200

    feat: fix file not found error

--- vec_db.py
+++ vec_db.py
@@ -109,26 +109,26 @@ class VecDB:
         if num_records == 10**6:
             available_ram = 20*10**6
             scaling_factor = 500
-            nprobe_1 = 30
-            nprobe_2 = 20
+            nprobe_1 = 100
+            nprobe_2 = 50
             
         elif num_records == 10*10**6:
             available_ram = 50*10**6
             scaling_factor = 1000
-            nprobe_1 = 50
-            nprobe_2 = 20
+            nprobe_1 = 200
+            nprobe_2 = 100
 
         elif num_records == 15*10**6:
             available_ram = 50*10**6
             scaling_factor = 2000
-            nprobe_1 = 50
-            nprobe_2 = 20
+            nprobe_1 = 300
+            nprobe_2 = 150
         
         elif num_records == 20*10**6:
             available_ram = 50*10**6
             scaling_factor = 10000
-            nprobe_1 = 20
-            nprobe_2 = 10
+            nprobe_1 = 400
+            nprobe_2 = 200
 
             
         vector_size_bytes = DIMENSION * ELEMENT_SIZE  
@@ -145,6 +145,8 @@ class VecDB:
         for _,_,i in nearest_neighbors_1:
 
             file_path = os.path.join(self.index_file, f'{i}.dat')
+            if not os.path.exists(file_path):
+                continue
             with open(file_path, 'rb') as index_file:
                 index = pickle.load(index_file)
                 
