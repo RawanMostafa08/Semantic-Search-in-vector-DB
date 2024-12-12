b17e3927a1db84cb217b3428c1c168cfcd3eb6b7
RawanMostafa08 <rawanmostafa401@gmail.com>
2024-12-12 17:57:36 +0200

    fix: change scaling factors

--- vec_db.py
+++ vec_db.py
@@ -114,7 +114,7 @@ class VecDB:
             
         elif num_records == 10*10**6:
             available_ram = 50*10**6
-            scaling_factor = 500
+            scaling_factor = 50
             nprobe_1 = 100
             nprobe_2 = 50
 
@@ -126,7 +126,7 @@ class VecDB:
         
         elif num_records == 20*10**6:
             available_ram = 50*10**6
-            scaling_factor = 4000
+            scaling_factor = 50
             nprobe_1 = 300
             nprobe_2 = 150
 
